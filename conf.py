14f9852daa0e7e5ee54ad78b10a39ee05c68544f
oliverpool <oliverpool@hotmail.fr>
2016-01-20 10:34:02 +0100

    Remove cover image

--- conf.py
+++ conf.py
@@ -126,7 +126,7 @@ html_title = "Patacrep " + release
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "img/svg/cover-guitar.svg"
+#html_logo = "img/svg/cover-guitar.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
